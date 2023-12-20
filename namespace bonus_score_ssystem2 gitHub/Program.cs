03b350362510090c112f4471c4042349e5f74ebf
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-12-20 11:51:37 +0200

    bonus score

---
+++ namespace bonus_score_ssystem2 gitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace namespace_bonus_score_ssystem2_gitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
