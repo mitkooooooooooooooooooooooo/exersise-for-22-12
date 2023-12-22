2a047b47acee93a6420412f52a1931c795ef6b77
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-12-22 18:46:41 +0200

    black flag

---
+++ black flag/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace black_flag
+{
+    class Program
+    {
+        static void Main()
+        {
+            int days = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double totalPlunder = 0;
+
+            for (int day = 1; day <= days; day++)
+            {
+                totalPlunder += dailyPlunder;
+
+                if (day % 3 == 0)
+                {
+                    totalPlunder += 0.5 * dailyPlunder;
+                }
+
+                if (day % 5 == 0)
+                {
+                    totalPlunder -= 0.3 * totalPlunder;
+                }
+            }
+
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:F2} plunder gained.");
+            }
+            else
+            {
+                double percentage = (totalPlunder / expectedPlunder) * 100;
+                Console.WriteLine($"Collected only {percentage:F2}% of the plunder.");
+            }
+        }
+    }
+
+}
