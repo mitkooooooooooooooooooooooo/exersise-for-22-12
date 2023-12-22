d2edd808d9a238d7fda68716bb133f3947ee68d7
mitkooooooooooooooooooooooo <146818015+mitkooooooooooooooooooooooo@users.noreply.github.com>
2023-12-22 18:25:24 +0200

    CSGO BOIIIIIIIII

---
+++ CSGO problem/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CSGO_problem
+{
+    class Program
+    {
+        static void Main()
+        {
+            int initialEnergy = int.Parse(Console.ReadLine());
+
+            int wonBattles = 0;
+
+            string input;
+            while ((input = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(input);
+
+                if (initialEnergy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with  {wonBattles} won battles and {initialEnergy} energy");
+                    return;
+                }
+
+                initialEnergy -= distance;
+                wonBattles++;
+
+                if (wonBattles % 3 == 0)
+                {
+                    initialEnergy += wonBattles;
+                }
+            }
+
+            Console.WriteLine($"Won battles: {wonBattles} Energy left: {initialEnergy}");
+        }
+    }
+
+}
